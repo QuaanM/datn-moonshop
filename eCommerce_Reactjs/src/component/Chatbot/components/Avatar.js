8639ebd9a5deada22d1239258e6c407954c8b909
QuaanM <quanphi2k2@gmail.com>
2024-12-28 13:42:16 +0700

    datn

---
+++ eCommerce_Reactjs/src/component/Chatbot/components/Avatar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Avatar() {
+    return (
+        <div className='chatbot-icon'>
+          <img src='/resources/img/logo7.png' alt='Chatbot' />
+        </div>
+      );
+    }
+    
